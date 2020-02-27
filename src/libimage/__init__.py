cd3b77ae8e837580e59aa8b310330afe320d1634
Daniel Ching <carterbox@users.noreply.github.com>
2020-02-27 14:12:32 -0600

    BUG: Use absolute file paths and remove stray =

--- src/libimage/__init__.py
+++ src/libimage/__init__.py
@@ -8,14 +8,19 @@ https://creativecommons.org/licenses/by-sa/3.0/deed.en] which means they may be
 used for free and for whatever purpose. Citation information is included in the
 citations.bib file. Each image has a separate attribution.
 """
-
+import lzma
+import os.path
+import pickle
 import warnings
+
 import numpy as np
+
+__root__ = os.path.dirname(__file__)
 
 def _downsample(rgb, size):
     """Shrink the 2048 x 2048 rgb image to size  x size by averaging pixels."""
     rgb = rgb.reshape(size, 2048 // size, size, 2048 // size, 3)
-    return = np.mean(rgb, axis=(1, 3))
+    return np.mean(rgb, axis=(1, 3))
 
 def load(name, size=2048, color=False):
     """Return an image from the library by name.
@@ -34,7 +39,8 @@ def load(name, size=2048, color=False):
                   "details.")
     if size > 2048 or 2048 % size != 0:
         raise ValueError('size can only be powers of 2 <= 2048.')
-    with lzma.open(f"{name}.lzma.p", "rb") as f:
+    filename = os.path.join(__root__, f"{name}.lzma.p")
+    with lzma.open(filename, "rb") as f:
         rgb = pickle.load(f) / 255
     rgb = _downsample(rgb, size)
     if color:
@@ -45,5 +51,6 @@ def _save(name):
     """Convert an RGB PNG to LZMA compressed pickle."""
     import skimage.io
     rgb = skimage.io.imread(f'{name}-2048.png')
-    with lzma.open(f"{name}.lzma.p", "wb") as f:
+    filename = os.path.join(__root__, f"{name}.lzma.p")
+    with lzma.open(filename, "wb") as f:
         pickle.dump(rgb, f)
